dadfaa40abbbfd3dc7140bf91fe8d8777a5a0253
Evgeny Mitko <em0072@gmail.com>
2018-06-16 23:16:03 +0300

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Evgeny Mitko on 16.06.2018.
+//  Copyright © 2018 Evgeny Mitko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
